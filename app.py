385a2389dc84e13abd4ce2bd4dec18a9013a38a6
andy matthews <andy@commadelimited.com>
2015-01-08 06:35:12 -0600

    Initial commit, step 01

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import (
+    Flask
+)
+
+app = Flask('__name__')
+app.debug = True
+
+
+
+# Run application
+if __name__ == '__main__':
+    # from os import environs
+    # app.run(debug=False, port=environ.get("PORT", 5000), processes=2)
+    app.run(debug=True, port=12345)
---
+++ readme.md
@@ -0,0 +1,4 @@
+# A Quick Sip from the Flask Microframework
+## PyTn companion repository
+
+Clone this repo, run `git branch` to list branches. Checkout `step_01` to begin.
