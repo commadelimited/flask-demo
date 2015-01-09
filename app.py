26c9509505e98372304a5762cf593a2a62fef3cf
andy matthews <andy@commadelimited.com>
2015-01-08 22:24:29 -0600

    Stub out additional routes

--- app.py
+++ app.py
@@ -5,10 +5,53 @@ from flask import (
 app = Flask('__name__')
 app.debug = True
 
+@app.route("/")
+def homepage():
+    return """
+    <h1>
+        Welcome to PyTennessee!<br />
+        The home page says hello!
+    </h1>
+    """
 
+@app.route("/news/")
+def news():
+    return """
+    <h1>
+        PyTennessee 2015!<br />
+        All the news that's fit to print.
+    </h1>
+    """
+
+@app.route("/schedule/")
+def schedule():
+    return """
+    <h1>
+        PyTennessee Conference Schedule!<br />
+        Tough decisions all around.<br />
+        But you, you chose wisely.
+    </h1>
+    """
+
+@app.route("/sponsors/")
+def sponsors():
+    return """
+    <h1>
+        PyTennessee Sponsors!<br />
+        The ones with the bucks<br />
+        And the bags and stickers and shirts!
+    </h1>
+    """
+
+@app.route("/conduct/")
+def conduct():
+    return """
+    <h1>
+        PyTennessee Rules of the road!<br />
+        Be excellent to each other.<br />
+    </h1>
+    """
 
 # Run application
 if __name__ == '__main__':
-    # from os import environs
-    # app.run(debug=False, port=environ.get("PORT", 5000), processes=2)
     app.run(debug=True, port=12345)
